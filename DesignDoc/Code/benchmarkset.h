9b197f90878d0035383777ec28b62b27af2807db
James Anastasiou <james.anastasiou@gmail.com>
2016-09-30 15:27:48 +1000

    Spellchecked

--- DesignDoc/Code/benchmarkset.h
+++ DesignDoc/Code/benchmarkset.h
@@ -1,16 +1,16 @@
-class BenchmarkSet
+struct BenchmarkSet
 {
-public:
     BenchmarkSet(std::string benchmarkLocation);
-    BenchmarkSet();
-    bool Exists(std::string operation) const;
-    double Speedup(std::string operation, std::size_t dimension) const;
-    double Speedup(std::string operation) const;
-    std::tuple<double, double> GetResult(std::string operation, std::size_t dimension) const;
-    std::tuple<double, double> LowerUpper(std::string operation, std::size_t dimension) const;
+    BenchmarkSet(const BenchmarkSet &rhs);
+    BenchmarkSet operator= (const BenchmarkSet &rhs);
+    ~BenchmarkSet();
+    bool Exists(std::string operation);
+    double Speedup(std::string operation, std::size_t dimension);
+    double Speedup(std::string operation);
+    std::tuple<double, double> GetResult(std::string operation, std::size_t dimension);
+    std::tuple<double, double> LowerUpper(std::string operation, std::size_t dimension);
 
-private:
-    std::map<std::string, std::map<std::size_t, std::tuple<double, double>>> benchmarks;
-    static const std::map<const std::string, const std::size_t> VectorFixtures;
-    static const std::map<const std::string, const std::size_t> MatrixFixtures;
+    std::map<std::string, std::map<std::size_t, std::tuple<double, double>>> benchmarks;;
+    static std::map<std::string, std::size_t> VectorFixtures;
+    static std::map<std::string, std::size_t> MatrixFixtures;
 };
